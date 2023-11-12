51ffda175a52130b3261506261d2228fbf67c844
Firdavs Nazarov <fredrickuzb55@gmail.com>
2023-11-12 22:17:26 +0900

    basic_Java

---
+++ Section40/Array.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Array {
+
+    public static void main(String[] args) {
+        
+        int[] example = new int[5];
+
+        example[1] = 2;
+        example[0] = 3;
+        example[4] = 5;
+        
+        System.out.println(Arrays.toString(example));
+
+    }
+    
+}
